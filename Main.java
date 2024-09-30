0a01693fbe7c3ce1932e00685feeab577ae80c5f
JL Gardiola <88923726+jlgardiola18@users.noreply.github.com>
2024-09-30 07:03:48 +0000

    done

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What is your hobby? ");
+        String hobby = scanner.nextLine();
+
+
+        System.out.println("Hello "+name);
+        System.out.println("You are "+age+" years old");
+        System.out.println("You like "+hobby);
+
+
+
+
     }
 }
